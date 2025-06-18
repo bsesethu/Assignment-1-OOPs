cc4e41be9e7e124942a11f647f59fc5fd8251c17
Sesethu_Bango <bsesethu@yahoo.com>
2025-06-18 13:32:50 +0200

    Cleaning 1

---
+++ cleaning_script.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+# Converting '.txt' to '.csv'
+# input_file = 'student_habits_performance_comma.txt'
+# output_file = 'student_habits_performance_commas.csv'
+
+# df1 = pd.read_csv(input_file, sep= '\t')
+# df1.to_csv(output_file, index= False)
+
+# Read the CSV
+# df = pd.read_csv('student_habits_performance_commas.csv')
+# print(df)
+
+# Read CSV with delimiter (;)
+df2 = pd.read_csv('student_habits_performance.csv', delimiter= ';')
+print(df2)  
+        # All the issues I was facing are now irrelevant, the string formatting issue I was seeing. I suppose sting values don't need to be in quotations
+print(df2.columns)
+
+
